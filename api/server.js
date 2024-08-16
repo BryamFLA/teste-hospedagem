757f278ce155f6c5d686376484c39dfb63532a4a
BryamFLA <103959230+BryamFLA@users.noreply.github.com>
2024-08-15 22:58:41 -0300

    att user/store e address

--- api/server.js
+++ api/server.js
@@ -27,6 +27,31 @@ app.post('/login', async (req, res)=>{
     }
 });
 
+app.post('/address', async (req, res) => {
+    const addressInfos = req.body;
+    console.log(addressInfos);
+
+    const id = 1;
+
+    res.status(201).json({message: 'successfully', id: id});
+});
+
+app.post('/store', async (req, res) => {
+    const { email, password } = req.body;
+
+    users.push({email: email, password: password, isUser: false});
+
+    res.status(201).json({message: 'successfully'});
+});
+
+app.post('/user', async (req, res) => {
+    const { email, password } = req.body;
+
+    users.push({email: email, password: password, isUser: true});
+
+    res.status(201).json({message: 'successfully'});
+});
+
 app.get('/teste', async (req, res)=>{
     res.status(200).json({message: 'deu boa'});
 });
