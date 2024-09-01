0e1c0f615d590eb40c38dfa6fa16141e86eb6f22
BryamFLA <103959230+BryamFLA@users.noreply.github.com>
2024-09-01 20:46:09 -0300

    att limite do backend

--- api/server.js
+++ api/server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 
 const users = [
     { email: 'user@teste.com', password: '123456789', isUser: true },
